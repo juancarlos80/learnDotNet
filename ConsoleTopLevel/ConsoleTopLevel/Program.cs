c9a45959e2b4dc935d271c812c02e48a850c74f5
juancarlos80 <juan.k.80@gmail.com>
2021-06-22 10:15:33 -0400

    Contact and Interpolation

--- ConsoleTopLevel/ConsoleTopLevel/Program.cs
+++ ConsoleTopLevel/ConsoleTopLevel/Program.cs
@@ -22,3 +22,13 @@ Console.WriteLine("\u3053 \u3093 \u306B \u3061 \u306F World!");
 // Nihon no seikyū-sho o seisei suru ni wa:
 Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
 Console.WriteLine(@"c:\invoices\app.exe -j");
+
+//String interpolation combined with verbatim
+string projectNam = "First-Project";
+Console.WriteLine($@"C:\Output\{projectNam}\Data");
+
+string projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+Console.WriteLine($"View English output:\n\t\tc:\\Exercise\\{projectName}\\data.txt\n");
+Console.WriteLine(russianMessage + ":\n\t\tc:\\Exercise\\{projectName}\\ru-RU\\data.txt\n");
