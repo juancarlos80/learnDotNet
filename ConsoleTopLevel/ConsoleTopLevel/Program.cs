23706d345279daa78c9a7c77751c65a5a006c6f5
juancarlos80 <juan.k.80@gmail.com>
2021-06-22 09:59:12 -0400

    Escape characters

--- ConsoleTopLevel/ConsoleTopLevel/Program.cs
+++ ConsoleTopLevel/ConsoleTopLevel/Program.cs
@@ -4,3 +4,21 @@ double numberPi = Math.PI;
 
 Console.WriteLine($"PI: {numberPi}");
 
+//Escape Characteres
+Console.WriteLine("Generating invoices for customer \"ABC Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+
+//Verbatim string literal
+Console.WriteLine(@"   c:\source\repos   
+      (this is where your code goes)");
+
+//Unicode Escape
+// Kon'nichiwa World
+Console.WriteLine("\u3053 \u3093 \u306B \u3061 \u306F World!");
+
+// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+Console.WriteLine(@"c:\invoices\app.exe -j");
